8141651f3ff6f8608810d1bc764a5b87e6df0c33
goswamiamit095-hub <goswamiamit095@gmail.com>
2026-06-15 13:12:36 +0530

    Update export.js

--- js/export.js
+++ js/export.js
@@ -1,1 +1,77 @@
+function exportToExcel(){
 
+    const wb =
+    XLSX.utils.book_new();
+
+    const ws =
+    XLSX.utils.json_to_sheet(
+        filteredData
+    );
+
+    XLSX.utils.book_append_sheet(
+        wb,
+        ws,
+        "SalesData"
+    );
+
+    XLSX.writeFile(
+        wb,
+        "SalesDashboard.xlsx"
+    );
+
+}
+
+function exportToCSV(){
+
+    const ws =
+    XLSX.utils.json_to_sheet(
+        filteredData
+    );
+
+    const csv =
+    XLSX.utils.sheet_to_csv(
+        ws
+    );
+
+    const blob =
+    new Blob(
+        [csv],
+        {
+            type:"text/csv"
+        }
+    );
+
+    const url =
+    URL.createObjectURL(blob);
+
+    const a =
+    document.createElement("a");
+
+    a.href = url;
+
+    a.download =
+    "SalesDashboard.csv";
+
+    a.click();
+
+    URL.revokeObjectURL(url);
+
+}
+
+document
+.getElementById(
+    "exportExcelBtn"
+)
+.addEventListener(
+    "click",
+    exportToExcel
+);
+
+document
+.getElementById(
+    "exportCsvBtn"
+)
+.addEventListener(
+    "click",
+    exportToCSV
+);
